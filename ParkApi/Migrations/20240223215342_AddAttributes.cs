d54c6a1ab640a5615fcd1c180358ed7f9c8be59e
Kim Robinson <kimmykokonut@gmail.com>
2024-02-23 13:54:42 -0800

    add attributes to park model properties

---
+++ ParkApi/Migrations/20240223215342_AddAttributes.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParkApi.Migrations
+{
+    public partial class AddAttributes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "State",
+                keyValue: null,
+                column: "State",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "State",
+                table: "Parks",
+                type: "varchar(2)",
+                maxLength: 2,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "Name",
+                keyValue: null,
+                column: "Name",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Parks",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "Designation",
+                keyValue: null,
+                column: "Designation",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Designation",
+                table: "Parks",
+                type: "varchar(7)",
+                maxLength: 7,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "State",
+                table: "Parks",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(2)",
+                oldMaxLength: 2)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Parks",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldMaxLength: 255)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Designation",
+                table: "Parks",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(7)",
+                oldMaxLength: 7)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
--- ParkApi/Models/Park.cs
+++ ParkApi/Models/Park.cs
@@ -1,12 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ParkApi.Models;
 
 public class Park
 {
   public int ParkId { get; set; }
+  [Required]
+  [StringLength(255)]
   public string Name { get; set; }
+  [Required]
+  [StringLength(7)]
   public string Designation { get; set; }
   public string Description { get; set; }
   public string City { get; set; }
+  [Required]
+  [StringLength(2)]
   public string State { get; set; }
   public string WebUrl { get; set; }
   public string PhotoUrl { get; set; }
@@ -14,5 +22,6 @@ public class Park
   public string FeeType { get; set; }
   public bool Campground { get; set; }
   public string LatLong { get; set; }
+  [Range(1872, 2024, ErrorMessage = "No parks were established in the US until 1872.")]
   public int YearEst { get; set; }
 }
