82aea38885b808268d0b2ba6ff552438849ec53e
Kim Robinson <kimmykokonut@gmail.com>
2024-02-23 11:03:52 -0800

    add get request route for all parks and park by id

---
+++ ParkApi/Controllers/ParksController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParkApi.Models;
+
+namespace ParkApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ParksController : ControllerBase
+  {
+    private readonly ParkApiContext _db;
+    public ParksController(ParkApiContext db)
+    {
+      _db = db;
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Park>>> Get()
+    {
+      return await _db.Parks.ToListAsync();
+    }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Park>> GetPark(int id)
+    {
+      Park park = await _db.Parks.FindAsync(id);
+
+      if (park == null)
+      {
+        return NotFound();
+      }
+      return park;
+    }
+  }
+}
